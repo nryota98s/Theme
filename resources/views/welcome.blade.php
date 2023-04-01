c90ab46dd5935bd7e1238aee17262ca397de55e2
“nryota98s” <ryoutanumamura@gmail.com>
2023-04-01 17:54:31 +0900

    最終課題確認用

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <h1>テスト用</h1>
+        <h1>テスト</h1>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
