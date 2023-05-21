189b370de6079429dbed92b8ee32f7963239ea05
WatanabeKazuma31 <k.15ka___zu@softbank.ne.jp>
2023-05-22 06:59:39 +0900

    変更

--- resources/views/createform.blade.php
+++ resources/views/createform.blade.php
@@ -15,7 +15,7 @@
 <div class="form-group">
   {!! Form::hidden('hidden', 'user_id',Auth::user()->user_id, ) !!}
 
-  <p>ユーザー名：{{ Auth::user()->name }}</p>
+  {!! Form :: input ( ' text ' , ' name ' ,Auth :: user () -> name , [ ' required ' , ' class ' => [ ' form-control ' , ' form_n ' ], ' placeholder ' => 'ユーザー名' ]) !!}
 
   {!! Form::input('text', 'newPost', null, ['required', 'class' => ['form-control','form_p'], 'placeholder' => '投稿内容']) !!}
 
